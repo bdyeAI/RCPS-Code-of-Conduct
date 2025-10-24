8edbc29e57ead05fba0f1c6f9bdce25ec48413fe
bdyeAI <bdye@rcps.org>
2025-10-24 15:23:53 -0400

    Update service-worker.js
    version 4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'conduct-cache-v3';
+const CACHE = 'conduct-cache-v4';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./data.json','./manifest.webmanifest','./assets/logo.png'];
 
 self.addEventListener('install', (e) => {
